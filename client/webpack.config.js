3ef314f8ae5b8f432e304d5c720d133f94b232cd
dearg-amadaun <83721789+dearg-amadaun@users.noreply.github.com>
2021-10-15 17:05:04 -0400

    trying to debug webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -62,12 +62,20 @@ module.exports = () => {
           loader: 'file-loader',
         },
         {
+          // test: /\.m?js$/,
+          // exclude: /(node_modules|bower_components)/,
+          // use: {
+          //   loader: 'babel-loader',
+          //   options: {
+          //     presets: ['@babel/preset-env']
           test: /\.m?js$/,
-          exclude: /(node_modules|bower_components)/,
+          exclude: /node_modules/,
+          // We use babel-loader in order to use ES6.
           use: {
             loader: 'babel-loader',
             options: {
-              presets: ['@babel/preset-env']
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
             },
           },
         },
