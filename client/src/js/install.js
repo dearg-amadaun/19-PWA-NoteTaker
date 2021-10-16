0e28142234b4a926bba949ade2fd1fd4bd86cd39
dearg-amadaun <83721789+dearg-amadaun@users.noreply.github.com>
2021-10-16 14:43:44 -0400

    debug install.js

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -8,14 +8,14 @@ window.addEventListener('beforeinstallprompt', (event) => {
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {
+Install.addEventListener('click', async () => {
     const promptEvent = window.deferredPrompt;
     if (!promptEvent)  {
         return;
     }
     promptEvent.prompt();
     window.deferredPrompt = null;
-    butInstall.classList.toggle('hidden', true);
+    Install.classList.toggle('hidden', true);
 });
 
 // TODO: Add an handler for the `appinstalled` event
